66985229de5f4ab1ca4fe0766d9ca5faf67716eb
David <davidongaroh@gmail.com>
2023-03-28 21:01:37 +0300

    populate Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Sample Portfolio</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
